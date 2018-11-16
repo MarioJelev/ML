31e8d24f5afe065ddcc6777f7fe0317e147ec362
Mario Jelev <mario.jelev@yahoo.com>
2018-11-15 18:27:34 -0600

    updated code

--- 6375_Assignment4/Assignment4_P2.py
+++ 6375_Assignment4/Assignment4_P2.py
@@ -9,6 +9,8 @@ class Tweet(object):
 		return 'Tweet (id=%s, text=%s)' % (self.id, self.text)
 	def get_id(self):
 		return self.id
+	def get_text(self):
+		return self.text
 
 def json_tweet_reader(file):
 	# instantiate list of tweets
@@ -43,14 +45,46 @@ def get_seed_tweets(seed_ids, tweets):
 	# print(seed_tweets)
 	return seed_tweets
 
+def compute_jaccard(tweet1, tweet2):
+	t1_list = tweet1.split(' ')
+	t2_list = tweet2.split(' ')
+	intersect = list(set(t1_list) & set(t2_list))
+	union = list(set(t1_list) | set(t2_list))
+	distance = 1 - (float(len(intersect)) / len(union))
+	return distance
 
-def get_clusters(tweets, seeds):
+def computeNewCentroids(clusters):
+	for cluster in clusters:
+		tweetsInCluster = clusters[cluster]
+		
+
+def get_clusters(tweets, seeds, clusters):
+	newClusters = {}
 	for tweet in tweets:
+		distanceToSeeds = {}
 		for seed in seeds:
-			compute_jaccard(tweet, seed)
+			# find the distance between the tweet and the seed
+			dist = compute_jaccard(tweet.get_text(), seed.get_text())
+			distanceToSeeds[seed.get_id()] = dist
+
+		tweet_cluster = min(distanceToSeeds, key=distanceToSeeds.get)
+		if tweet_cluster not in newClusters:
+			newClusters[tweet_cluster] = [tweet.get_id]
+		else:
+			newClusters[tweet_cluster].append(tweet.get_id)
+	if newClusters == clusters:
+		return newClusters
+	else:
+		newClusters = computeNewCentroids(newClusters)
+		# print (tweet.get_id(), distanceToSeeds)
+	# compute_jaccard('the long march', 'ides of march')
 
 
 tweets = json_tweet_reader('Tweets.json')
 seed_ids = txt_seed_reader('InitialSeeds.txt')
 seed_tweets = get_seed_tweets(seed_ids, tweets)
-get_clusters(tweets, seed_tweets)
+#initialize dictionary with the keys as the seeds
+clusters = {}
+for id in seed_ids:
+	clusters[id] = []
+get_clusters(tweets, seed_tweets, clusters)
